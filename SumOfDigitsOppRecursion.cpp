e07f3ec0381a8a25e2523c43e3876d2ca9aa6786
Prabhat Kadam <prabhatkadam18@gmail.com>
2019-02-28 15:42:12 +0530

    Sum of Digits 2 WORKING

--- SumOfDigitsOppRecursion.cpp
+++ SumOfDigitsOppRecursion.cpp
@@ -1,36 +1,32 @@
 //
 //  SumOfDigitsOppRecursion.cpp
-//  
+//
 //
 //  Created by Prabhat Kadam on 01/02/19.
 //
 
-
-
-//    WORKING BUT NOT HOW IT SHOULD
-
-
-
 #include <iostream>
 using namespace std;
 
-int fun(int n,int x)
+int fun(int n)
 {
-    if(n/10==0)
+    if(n==0)
     {
-        return x+n;
+        return 0;
     }
     else
     {
-        x=x+n%10;
-        x=fun(n/10,x);
-        return x;
+        int r=n%10;
+        int sum=r+fun(n/10);
+        cout<<r<<" ";
+        return sum;
     }
 }
 
 int main()
 {
     int a=12134;
-    cout<<fun(a,0);
+    int sum=fun(a);
+    cout<<endl<<"Sum is: "<<sum;
     return 0;
 }
